0624eba4b894c7dab6fe6e5a263aeaabc9b866a5
sapeg <serggio8888@gmail.com>
2021-03-12 09:50:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework_5_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Подсчет гласных в строке
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            char[] ch = { 'а', 'у', 'е', 'ы', 'о', 'э', 'я', 'и', 'ю', 'У', 'Е', 'Ы', 'А', 'О', 'Э', 'Я', 'И', 'Ю', };
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < ch.Length; j++)
+                {
+                    if (str[i] == ch[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
